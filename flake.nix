565652c758be2d2b5db8af8ab83a485608b7a5ae
tonakai-s <rdorneles64@gmail.com>
2026-02-14 17:13:59 -0300

    first commit

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Flakes file from Leptos docs";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs =
+            [
+              pkg-config
+              trunk
+              lld
+              leptosfmt
+              (rust-bin.stable.latest.default.override {
+                  extensions = [
+                    "rust-src"
+                    "rust-analyzer"
+                  ];
+                  targets = [ "wasm32-unknown-unknown" ];
+                }
+              )
+            ];
+          shellHook = '''';
+        };
+      }
+    );
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+use leptos::prelude::*;
+
+fn main() {
+    leptos::mount::mount_to_body(|| view! { <p>
+        "Hello, world!"</p> })
+}
