5531c92804b77828041020831337b6ef5b3253bc
tonakai-s <rdorneles64@gmail.com>
2026-02-17 09:28:20 -0300

    feat: First signal reactivity

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
-use leptos::prelude::*;
+use leptos::{
+    component,
+    mount::mount_to_body,
+    prelude::{signal, ElementChild, Get, OnAttribute, Write},
+    view, IntoView,
+};
 
 fn main() {
-    leptos::mount::mount_to_body(|| view! { <p>
-        "Hello, world!"</p> })
+    mount_to_body(App)
+}
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = signal(0);
+
+    view! {
+        // {count} is a signal
+        // 'count.get' get the inner value of the signal
+        // 'set_count.set()' is used to set a fixed value, '.write()' updates the value 'in-place'
+        <button on:click=move |_| *set_count.write() += 1>"Click me: " {count}</button>
+        <p>"Double count: " {move || count.get() * 2}</p>
+    }
 }
